f828ac10526b8de2663b5bb8130ebdce590a6235
Joaquin Owono Afugu Ntoo <jafuguntoo@pvamu.edu>
2025-10-15 01:43:32 -0500

    Resolve merge conflicts: combine greetings, intro, and goals

--- HWorld.py
+++ HWorld.py
@@ -1,22 +1,14 @@
 print("Hello, World!")
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("Hi!, Conflict solved")
 
-intro = input ("Please, introduce yourself")
-goals = input ("Enter your goals:")
-print (f"{intro}")
-print(f"{goals}")
 
-=======
+print("Hi! Joaquin Owono")
+print("Hi! Feature-2 Version")
 
 
 intro = input("Please, introduce yourself: ")
 print(f"Introduction: {intro}")
->>>>>>> feature-introduction
-=======
 
 
 goals = input("Enter your goals: ")
 print(f"Goals: {goals}")
->>>>>>> feature-goals
